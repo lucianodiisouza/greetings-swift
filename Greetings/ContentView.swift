ae8705cde224321538478dea13f46004cfa5877e
Luciano dii Souza <luciano.desouza@workl.com>
2024-08-06 20:12:18 -0300

    feat: testing stack and properties

--- Greetings/ContentView.swift
+++ Greetings/ContentView.swift
@@ -9,13 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+//            Color
+//                .cyan
+//                .opacity(0.7)
+//                .ignoresSafeArea()
+            
+            LinearGradient(
+                colors: [.cyan, .yellow, .blue],
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            ).opacity(0.5)
+             .ignoresSafeArea()
+            
+            VStack {
+                Text("Greetings !")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .padding()
+                    .foregroundStyle(Color.purple)
+                    .background(Color.orange)
+                    .cornerRadius(20.0)
+                    .shadow(color: .orange, radius: 5, x: 5.0, y: 5)
+                    .padding()
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
