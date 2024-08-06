20b95d0c917694381e654ddd6e4baeb699be580b
Luciano dii Souza <luciano.desouza@workl.com>
2024-08-06 19:59:48 -0300

    Initial Commit

---
+++ Greetings/GreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsApp.swift
+//  Greetings
+//
+//  Created by Luciano dii Souza on 06/08/24.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
