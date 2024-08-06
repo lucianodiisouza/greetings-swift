b5a66af1dd0f380863515d7429aa9f40312e9f58
Luciano dii Souza <luciano.desouza@workl.com>
2024-08-06 20:37:34 -0300

    applying TextView on ContentView

--- Greetings/ContentView.swift
+++ Greetings/ContentView.swift
@@ -10,45 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack(alignment: .leading) {
-                Text("Hello There")
-                    .fontWeight(.semibold)
-                    .padding()
-                    .foregroundStyle(Color.white)
-                    .background(Color.orange.opacity(0.5))
-                    .cornerRadius(20.0)
-                    .shadow(color: .orange, radius: 5, x: 10, y: 10)
-                
-                Text("Hi")
-                    .fontWeight(.semibold)
-                    .padding()
-                    .foregroundStyle(Color.white)
-                    .background(Color.green.opacity(0.5))
-                    .cornerRadius(20.0)
-                    .shadow(color: .orange, radius: 5, x: 10, y: 10)
-                Text("Ready?")
-                    .fontWeight(.semibold)
-                    .padding()
-                    .foregroundStyle(Color.white)
-                    .background(Color.purple.opacity(0.5))
-                    .cornerRadius(20.0)
-                    .shadow(color: .orange, radius: 5, x: 10, y: 10)
-                Text("Welcome,")
-                    .fontWeight(.semibold)
-                    .padding()
-                    .foregroundStyle(Color.white)
-                    .background(Color.gray.opacity(0.5))
-                    .cornerRadius(20.0)
-                    .shadow(color: .orange, radius: 5, x: 10, y: 10)
-                
-                Text("to Swift!")
-                    .fontWeight(.semibold)
-                    .padding()
-                    .foregroundStyle(Color.yellow)
-                    .background(Color.gray.opacity(0.5))
-                    .cornerRadius(20.0)
-                    .shadow(color: .orange, radius: 5, x: 10, y: 10)
-                    
+            TextView(text: "Hello There", color: .green)
+            TextView(text: "Welcome to Swift Programming", color: .purple)
+            TextView(text: "Are you ready to, ", color: .orange)
+            TextView(text: "start exploring?", color: .yellow)
+            TextView(text: "Boom!", color: .cyan)
             }
+        .padding()
     }
 }
 
