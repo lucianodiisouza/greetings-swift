e23227f3998ca1ab5d552c60192f4a22fdf332f0
Luciano dii Souza <lucianodiisouza@hotmail.com>
2024-08-06 21:39:53 -0300

    Create DataItemModel.swift

---
+++ Greetings/DataItemModel.swift
@@ -0,0 +1,13 @@
+//
+//  DataItemModel.swift
+//  Greetings
+//
+//  Created by Luciano dii Souza on 06/08/24.
+//
+
+import SwiftUI
+
+struct DataItemModel {
+    let text: String
+    let color: Color
+}
